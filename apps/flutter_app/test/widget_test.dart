3ff1e6b3084b8e4079aabd8771c5ea9d8f223d5b
BMFuture <279071304+Bintangmuda-new@users.noreply.github.com>
2026-09-04 06:53:57 +0700

    fix: import ValueKey in widget test

--- apps/flutter_app/test/widget_test.dart
+++ apps/flutter_app/test/widget_test.dart
@@ -1,5 +1,6 @@
 import 'dart:ui' show Size;
 
+import 'package:flutter/foundation.dart' show ValueKey;
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:bmfuture_po_autotrade/app.dart';
